abbf15c5df915be0453a4259664ad98d3be82238
spellcry <sangiothebest@hotmail.com>
2022-08-24 15:25:11 +0200

    add li, next, prev & vue obj

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,26 @@ const slides = [
 	'./img/04.jpg',
 	'./img/05.jpg',
 ]
+
+const app = new Vue({
+	el: '#main-content',
+	data: {
+		name: 'Vue Carousel',
+		slides,
+		idSlideActive: 0,
+	},
+	methods: {
+		nextSlide() {
+			if (this.idSlideActive + 1 < this.slides.length)
+				this.idSlideActive++;
+			else
+				this.idSlideActive = 0;
+			},
+		prevSlide() {
+			if (this.idSlideActive - 1 >= 0)
+				this.idSlideActive--;
+			else
+				this.idSlideActive = this.slides.length - 1;
+		}
+	}
+});
